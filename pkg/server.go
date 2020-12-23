48047704c1060afe7eabdbe4213eae1338c1ae7c
GLevis <levis.gavin@gmail.com>
2020-12-22 23:00:12 -0600

    added response

--- pkg/server.go
+++ pkg/server.go
@@ -37,7 +37,7 @@ func (s *Server) Run() error {
 		// multiple connections may be served concurrently.
 		go func(c net.Conn) {
 			// Process request
-
+			io.Copy(c, c)
 			// Shut down the connection.
 			c.Close()
 		}(conn)
